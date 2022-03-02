62a4b75084ac4669a67562354a05b1d89c21ef07
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-03-03 01:00:21 +0530

    v0.0.1

--- app/src/main/java/ss/dark/GitHub/MainActivity.java
+++ app/src/main/java/ss/dark/GitHub/MainActivity.java
@@ -1,5 +1,9 @@
 package ss.dark.GitHub;
 
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
 import ss.dark.GitHub.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,7 +14,5 @@ public class MainActivity extends AppCompatActivity {
 
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-
     }
-
 }
