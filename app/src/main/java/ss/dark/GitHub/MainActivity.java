634d18bc7e8572379dba016fce6c705fb67bb2c2
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-03-03 01:06:29 +0530

    v0.0.1

--- app/src/main/java/ss/dark/GitHub/MainActivity.java
+++ app/src/main/java/ss/dark/GitHub/MainActivity.java
@@ -8,9 +8,14 @@ import ss.dark.GitHub.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
