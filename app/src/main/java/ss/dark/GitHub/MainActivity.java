575bb547654cfb1a9cb7558825dc0b547862145a
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-03-03 13:08:12 +0530

    v0.0.4

--- app/src/main/java/ss/dark/GitHub/MainActivity.java
+++ app/src/main/java/ss/dark/GitHub/MainActivity.java
@@ -70,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         try {
             requestWindowFeature(Window.FEATURE_NO_TITLE);
-            //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
             setContentView(R.layout.activity_main);
             webView = findViewById(R.id.WebView);
             webSettings = webView.getSettings();
