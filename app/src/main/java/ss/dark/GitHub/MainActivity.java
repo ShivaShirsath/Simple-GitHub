e0fd6866327e7d83bb1b11309542d6240eda8d8f
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-07-15 13:43:18 +0530

    check

--- app/src/main/java/ss/dark/GitHub/MainActivity.java
+++ app/src/main/java/ss/dark/GitHub/MainActivity.java
@@ -72,6 +72,8 @@ public class MainActivity extends AppCompatActivity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         try {
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
             //hideSystemUI();
             setContentView(R.layout.activity_main);
             webView = findViewById(R.id.WebView);
